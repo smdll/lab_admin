7d7548656c840f868d9b1eeb1f3bfa76d85d1e9c
Songmingda <songmingda1234@hotmail.com>
2017-12-03 23:22:27 +0800

    modified:   GUI.py 	modified:   functions.py 	modified:   lab.db 	modified:   pre_repair.txt

--- GUI.py
+++ GUI.py
@@ -4,52 +4,41 @@ import wx.grid
 import functions
 
 class GridFrame(wx.Frame):
-	colLabel = (u'类别', u'设备名', u'型号', u'规格', u'单价', u'数量', u'购置日期', u'生产厂家', u'购买人', u'批次', u'机房', u'状态')
-	colName = ('Type', 'Name', 'Model', 'Spec', 'Cost', 'Date', 'Manuf', 'Resp', 'Batch', 'Room')
-	colSize = (80, 200, 50, 50, 50, 30, 70, 80, 55, 30, 55, 50)
-
 	def __init__(self, parent):
 		self.initUI(parent)
-		self.drawTable()
-		self.bindEvents()
 
 	# Initialize the graphical interface
 	def initUI(self, parent):
+		colLabel = (u'类别', u'设备名', u'型号', u'规格', u'单价', u'数量', u'购置日期', u'生产厂家', u'购买人', u'批次', u'机房', u'状态')
+		colSize = (80, 200, 50, 50, 50, 30, 70, 80, 55, 30, 55, 50)
 		width = 0
-		for i in self.colSize:
+		for i in colSize:
 			width += i + 1
 		wx.Frame.__init__(self, parent, title = u'实验室设备管理系统', size = (width, 360), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
 
 		self.btn1 = wx.Button(self, -1, u'设备入库', (0, 0), (80, 40))
 		self.btn2 = wx.Button(self, -1, u'维修管理', (90, 0), (80, 40))
 		wx.StaticText(self, -1, u'关键字：', (180, 10), (50, 30))
-		self.cb = wx.ComboBox(self, -1, pos = (230, 5), size = (100, 30), choices = [i for i in self.colLabel[:-1] if i != self.colLabel[5]])
+		self.cb = wx.ComboBox(self, -1, pos = (230, 5), size = (100, 30), choices = [i for i in colLabel[:-1] if i != colLabel[5]])
 		wx.StaticText(self, -1, u'排序：', (340, 10), (45, 30))
 		self.rb = wx.RadioBox(self, -1, pos = (375, -10), size = (50, 40), choices = [u'升序', u'降序'])
 		self.btn3 = wx.Button(self, -1, u'查询', (480, 0), (80, 40))
-
-		self.grid = wx.grid.Grid(self, -1, pos = (0, 40), size = (width, 360))
-		self.Show()
-
-	# Draw the table and put some data on
-	def drawTable(self):
 		self.db = functions.lab_db()
 
-		self.grid.CreateGrid(0, len(self.colLabel))
+		self.grid = wx.grid.Grid(self, -1, pos = (0, 40), size = (width, 360))
+		self.grid.CreateGrid(0, len(colLabel))
 		self.grid.SetRowLabelSize(0)
-
-		for i in range(len(self.colLabel)):
-			self.grid.SetColLabelValue(i, self.colLabel[i])
-			self.grid.SetColSize(i, self.colSize[i])
+		for i in range(len(colLabel)):
+			self.grid.SetColLabelValue(i, colLabel[i])
+			self.grid.SetColSize(i, colSize[i])
 
 		self.refreshData('id', 'ASC')
 
-	# Bind all events in need
-	def bindEvents(self):
 		self.Bind(wx.EVT_BUTTON, self.Warehouse, self.btn1)
 		self.Bind(wx.EVT_BUTTON, self.Repair, self.btn2)
 		self.Bind(wx.EVT_BUTTON, self.Query, self.btn3)
 		self.Bind(wx.EVT_CLOSE, self.Exit)
+		self.Show()
 
 	# Clear table and append new data
 	def refreshData(self, keyword, sort):
@@ -61,7 +50,7 @@ class GridFrame(wx.Frame):
 		j = 0
 		for line in data:
 			self.grid.InsertRows(j)
-			for i in range(len(self.colLabel)):
+			for i in range(len(line)):
 				if isinstance(line[i], int):
 					val = '%d'%line[i]
 				else:
@@ -72,8 +61,7 @@ class GridFrame(wx.Frame):
 
 	# Instrument warehousing interface
 	def Warehouse(self, event):
-		self.whf = wx.Dialog(self, -1, u'入库', size = (190, 330), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-		self.whf.ToggleWindowStyle(wx.STAY_ON_TOP)
+		self.whf = wx.Dialog(self, -1, u'入库', size = (190, 330), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX) | wx.FRAME_FLOAT_ON_PARENT)
 
 		wx.StaticText(self.whf, -1, u'类别：', pos = (0, 0), size = (70, 20))
 		self.whtype = wx.TextCtrl(self.whf, -1, pos = (70, 0), size = (100, 20))
@@ -102,14 +90,58 @@ class GridFrame(wx.Frame):
 		self.whf.Bind(wx.EVT_BUTTON, self.addHandle, self.whpostbtn)
 		self.whf.Show()
 
+	# incomplete
 	def Repair(self, event):
-		self.rf = wx.Dialog(self, -1, u'维修', size = (190, 330), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-		self.rf.ToggleWindowStyle(wx.STAY_ON_TOP)
+		colLabel = (u'编号', u'费用', u'维修日期', u'修理厂家', u'负责人', u'状态')
+		colSize = (30, 50, 70, 80, 55, 50)
+		width = 0
+		for i in colSize:
+			width += i + 1
+
+		self.rf = wx.Dialog(self, -1, u'维修', size = (width, 330), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX) | wx.FRAME_FLOAT_ON_PARENT)
+
+		wx.StaticText(self.rf, -1, u'损坏设备：', pos = (0, 5), size = (60, 20))
+		self.rcb = wx.ComboBox(self.rf, -1, pos = (60, 5), size = (60, 20), choices = self.db.repair_query_id())
+		wx.StaticText(self.rf, -1, u'费用：', pos = (130, 5), size = (50, 20))
+		self.rcost = wx.TextCtrl(self.rf, -1, pos = (180, 5), size = (50, 20))
+
+		wx.StaticText(self.rf, -1, u'维修日期：', pos = (0, 25), size = (60, 20))
+		self.rdate = wx.TextCtrl(self.rf, -1, pos = (60, 25), size = (80, 20))
+		wx.StaticText(self.rf, -1, u'修理厂家：', pos = (150, 25), size = (60, 20))
+		self.rserv = wx.TextCtrl(self.rf, -1, pos = (210, 25), size = (100, 20))
+
+		wx.StaticText(self.rf, -1, u'负责人：', pos = (0, 45), size = (50, 20))
+		self.rresp = wx.TextCtrl(self.rf, -1, pos = (50, 45), size = (70, 20))
+		self.rbtn = wx.Button(self.rf, -1, u'添加', pos = (130, 45), size = (50, 20))
+
+		self.rgrid = wx.grid.Grid(self.rf, -1, pos = (0, 65), size = (400, 360))
+		self.rgrid.CreateGrid(0, 6)
+		self.rgrid.SetRowLabelSize(0)
+
+		for i in range(len(colLabel)):
+			self.rgrid.SetColLabelValue(i, colLabel[i])
+			self.rgrid.SetColSize(i, colSize[i])
+
+		data = self.db.repair_query()
+		j = 0
+		for line in data:
+			self.rgrid.InsertRows(j)
+			for i in range(len(line)):
+				if isinstance(line[i], int):
+					val = '%d'%line[i]
+				else:
+					val = line[i]
+				self.rgrid.SetCellValue(j, i, val)
+				self.rgrid.SetReadOnly(j, i)
+			j += 1
+			
+		self.rf.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.fixHandle)
 		self.rf.Show()
 
 	# Sort and refresh data
 	def Query(self, event):
-		keyword = self.colName[self.cb.GetSelection()]
+		colName = ('Type', 'Name', 'Model', 'Spec', 'Cost', 'Date', 'Manuf', 'Resp', 'Batch', 'Room')
+		keyword = colName[self.cb.GetSelection()]
 		sort = ['ASC', 'DESC']
 		self.refreshData(keyword, sort[self.rb.GetSelection()])
 
@@ -136,6 +168,9 @@ class GridFrame(wx.Frame):
 		wx.MessageBox(u'添加成功', u'成功')
 		self.whf.Destroy()
 		self.Query(None)
+
+	def fixHandle(self, event):
+		print 'fix'
 
 	def Exit(self, event):
 		self.Destroy()
--- functions.py
+++ functions.py
@@ -30,14 +30,26 @@ class lab_db:
 		self.commit()
 
 	def inst_query(self, keyword, sort):
-		set = self.cur.execute('SELECT DISTINCT id,Name,Model,Batch FROM Main ORDER BY %s %s'%(keyword, sort)).fetchall()
+		set = self.cur.execute('SELECT DISTINCT Name,Model,Batch FROM Main WHERE id!=(SELECT id FROM Repair) ORDER BY %s %s'%(keyword, sort)).fetchall()
+		repair_set = self.cur.execute('SELECT id,Name,Model,Batch FROM Main WHERE id IN (SELECT id FROM Repair) ORDER BY %s %s'%(keyword, sort)).fetchall()
 		result = []
 		for i in set:
-			temp = list(self.cur.execute('SELECT Type,Name,Model,Spec,Cost,Count(Batch),Date,Manuf,Resp,Batch,Room FROM Main WHERE Name="%s" AND Model="%s" AND Batch=%s ORDER BY %s %s'%(i[1], i[2], i[3], keyword, sort)).fetchone())
-			status = self.cur.execute('SELECT Status FROM Repair WHERE id=%s'%(i[0])).fetchone()
-			if status == None:
-				temp.append(u'正常')
-			else:
-				temp.append(status[0])
+			temp = list(self.cur.execute('SELECT Type,Name,Model,Spec,Cost,Count(Batch),Date,Manuf,Resp,Batch,Room FROM Main WHERE Name="%s" AND Model="%s" AND Batch=%s'%(i[0], i[1], i[2])).fetchone())
+			temp.append(u'正常')
+			result.append(temp)
+		for i in repair_set:
+			temp = list(self.cur.execute('SELECT Type,Name,Model,Spec,Cost,Count(Batch),Date,Manuf,Resp,Batch,Room FROM Main WHERE Name="%s" AND Model="%s" AND Batch=%s'%(i[1], i[2], i[3])).fetchone())
+			temp.append(self.cur.execute('SELECT Status FROM Repair WHERE id=%d'%(i[0])).fetchone()[0])
 			result.append(temp)
 		return result
+
+	def repair_query_id(self):
+		data = self.cur.execute('SELECT id FROM Main WHERE id NOT IN (SELECT ID FROM Repair)').fetchall()
+		result = []
+		for i in data:
+			result.append('%d'%i[0])
+		return result
+
+	def repair_query(self):
+		data = self.cur.execute('SELECT * FROM Repair').fetchall()
+		return data
